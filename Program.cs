d4d5e6c6ae5dde92fc311d6cfce36b61e3aeb3fc
UwUontheweb <94400578+UWUontheweb@users.noreply.github.com>
2023-01-13 13:50:44 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace Program
 	{
 		staic void Main(string[] args)
 		{
-			Console.WriteLine("This is a thing");
+			Console.WriteLine("Would you like to start ?\(yes or no)");
+			
+			answer = Console.ReadLine();
+			
+			switch (answer) 
+			{
+				case "yes":
+					{
+						Console.WriteLine("Good\nLet us begin...");		
+					}
+			}
 		}
 	}
 }
